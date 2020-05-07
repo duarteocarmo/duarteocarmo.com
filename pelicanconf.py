1f31f0e50ecac4a154eb6cf91c43090c9d8a5996
Duarte OC <duarteocarmo@gmail.com>
2020-05-07 23:21:39 +0200

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import pelican
+
+AUTHOR = "Duarte O.Carmo"
+SITENAME = "duarte o.carmo"
+SITEURL = ""
+SITE_DESCRIPTION = "My personal website"
+# TODO
+SITELOGO = "images/logo.png"
+PATH = "content"
+TIMEZONE = "Europe/Copenhagen"
+DEFAULT_LANG = "en"
+
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}.html"
+ARTICLE_URL = "blog/{slug}.html"
+ARTICLE_SAVE_AS = "blog/{slug}.html"
+
+# SEO
+PELICAN_VERSION = pelican.__version__
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = "feed.xml"
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+# pagination
+DEFAULT_PAGINATION = False
+
+# extra paths
+STATIC_PATHS = ["images", "pdfs", "extra/favicon.ico", "pdfs/cv.pdf"]
+EXTRA_PATH_METADATA = {
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "pdfs/cv.pdf": {"path": "cv.pdf"},
+}
+
+# SOCIAL
+TWITTER_HANDLE = "duarteocarmo"
