961e6e8bf041b533c390fbce0a84b04b39d093a3
Duarte OC <duarteocarmo@gmail.com>
2020-05-21 13:22:48 +0200

    no logs

--- theme/static/js/dark_mode.js
+++ theme/static/js/dark_mode.js
@@ -14,15 +14,15 @@ const modeStatusElement = document.querySelector('.js-mode-status');
  * A reduced version of this: https://andy-bell.design/wrote/get-css-custom-property-value-with-javascript/
  */
 const getSSCustomProp = (propKey) => {
-	let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+    let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
 
-	// Tidy up the string if there’s something to work with
-	if (response.length) {
-		response = response.replace(/\'|"/g, '').trim();
-	}
+    // Tidy up the string if there’s something to work with
+    if (response.length) {
+        response = response.replace(/\'|"/g, '').trim();
+    }
 
-	// Return the string response by default
-	return response;
+    // Return the string response by default
+    return response;
 };
 
 /**
@@ -31,49 +31,48 @@ const getSSCustomProp = (propKey) => {
  * controlled by the media query
  */
 const applySetting = passedSetting => {
-	let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
+    let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
 
-	if(currentSetting) {
-		document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
-		setButtonLabelAndStatus(currentSetting);
-	}
-	else {
-		setButtonLabelAndStatus(getCSSCustomProp(COLOR_MODE_KEY));
-	}
+    if(currentSetting) {
+        document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
+        setButtonLabelAndStatus(currentSetting);
+    }
+    else {
+        setButtonLabelAndStatus(getCSSCustomProp(COLOR_MODE_KEY));
+    }
 }
 
 /**
  * Get’s the current setting > reverses it > stores it
  */
 const toggleSetting = () => {
-	let currentSetting = localStorage.getItem(STORAGE_KEY);
+    let currentSetting = localStorage.getItem(STORAGE_KEY);
 
-	switch(currentSetting) {
-		case null:
-			currentSetting = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
-			break;
-		case 'light':
-			currentSetting = 'dark';
-			break;
-		case 'dark':
-			currentSetting = 'light';
-			break;
-	}
-
-
-	localStorage.setItem(STORAGE_KEY, currentSetting);
-    console.log("switch theme: ", currentSetting);
+    switch(currentSetting) {
+        case null:
+            currentSetting = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+            break;
+        case 'light':
+            currentSetting = 'dark';
+            break;
+        case 'dark':
+            currentSetting = 'light';
+            break;
+    }
 
 
-	return currentSetting;
+    localStorage.setItem(STORAGE_KEY, currentSetting);
+
+
+    return currentSetting;
 }
 
 /**
  * A shared method for setting the button text label and visually hidden status element 
  */
 const setButtonLabelAndStatus = currentSetting => { 
-	modeToggleText.innerText = `${currentSetting === 'dark' ? '.' : '.'}`;
-//	modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
+    modeToggleText.innerText = `${currentSetting === 'dark' ? '.' : '.'}`;
+    //	modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
 }
 
 /**
@@ -81,18 +80,18 @@ const setButtonLabelAndStatus = currentSetting => {
  * from the toggle setting method.
  */
 modeToggleButton.addEventListener('click', evt => {
-	evt.preventDefault();
-	applySetting(toggleSetting());
+    evt.preventDefault();
+    applySetting(toggleSetting());
 });
 
 applySetting();
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-	const newColorScheme = e.matches ? "dark" : "light";
-	if (newColorScheme != localStorage.getItem(STORAGE_KEY)) {
-		e.preventDefault();
-		applySetting(toggleSetting());
+    const newColorScheme = e.matches ? "dark" : "light";
+    if (newColorScheme != localStorage.getItem(STORAGE_KEY)) {
+        e.preventDefault();
+        applySetting(toggleSetting());
 
-	} 
-	
+    } 
+
 });
