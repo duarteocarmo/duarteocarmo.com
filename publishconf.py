c9c3d92c07fd93977b40d4882785d7ba964d83b7
duarteocarmo <duarteocarmo@gmail.com>
2021-10-27 19:04:58 +0200

    added plausible

--- publishconf.py
+++ publishconf.py
@@ -14,11 +14,8 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://duarteocarmo.com"
 RELATIVE_URLS = False
-
 FEED_ALL_ATOM = "feed.xml"
-
 DELETE_OUTPUT_DIRECTORY = True
-
-GOOGLE_ANALYTICS = "G-B1G7KP3ZF4"
-GOAT_COUNTER = True
+GOOGLE_ANALYTICS = False 
+GOAT_COUNTER = False
 PLAUSIBLE = True
